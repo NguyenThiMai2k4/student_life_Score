7e2b43c20430cdd489c2f90d713b3dfd0e79f022
nglhongphuong <nglhongphuong@gmail.com>
2025-01-30 18:32:24 +0700

    home -> homescreen

---
+++ components/Home/DetailActivity.js
@@ -0,0 +1,16 @@
+import MyStyle from "../../styles/MyStyle"
+import {View,RefreshControl,  StatusBar, ScrollView, ImageBackground, Text, Image, TextInput, TouchableOpacity, SafeAreaView, StyleSheet, Pressable, ActivityIndicator, FlatList} from 'react-native';
+import Style from "./Style.js";
+import React from 'react'
+import APIs, { endpoints } from "../../configs/API";
+import { Chip, Searchbar, List } from 'react-native-paper';
+
+
+const DetailActivity = () =>
+{
+    return (
+        <Text style={Style.subject}>Hello</Text>
+    )
+}
+
+export default DetailActivity;
